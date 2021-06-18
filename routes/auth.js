9ffa916fa4168cf5678211988ed347e1034f169f
Jared Hanson <jaredhanson@gmail.com>
2021-06-18 10:28:53 -0700

    Provision user.

--- routes/auth.js
+++ routes/auth.js
@@ -11,9 +11,56 @@ router.get('/login', function(req, res, next) {
 router.get('/login/federated/accounts.google.com', passport.authenticate('google', { prompt: 'login' }));
 
 router.get('/oauth2/redirect/accounts.google.com', 
-  passport.authenticate('google', { failureRedirect: '/login' }),
-  function(req, res) {
-    res.redirect('/');
+  passport.authenticate('google', { assignProperty: 'federatedUser', failureRedirect: '/login' }),
+  function(req, res, next) {
+    console.log(req.federatedUser)
+    
+    db.get('SELECT * FROM federated_credentials WHERE provider = ? AND subject = ?', [
+      'https://accounts.google.com',
+      req.federatedUser.id
+    ], function(err, row) {
+      if (err) { return next(err); }
+      if (!row) {
+        db.run('INSERT INTO users (name) VALUES (?)', [
+          req.federatedUser.displayName
+        ], function(err) {
+          if (err) { return next(err); }
+          
+          var id = this.lastID;
+          db.run('INSERT INTO federated_credentials (provider, subject, user_id) VALUES (?, ?, ?)', [
+            'https://accounts.google.com',
+            req.federatedUser.id,
+            id
+          ], function(err) {
+            if (err) { return next(err); }
+            var user = {
+              id: id.toString(),
+              displayName: req.federatedUser.displayName
+            };
+            req.login(user, function(err) {
+              if (err) { return next(err); }
+              res.redirect('/');
+            });
+          });
+        });
+      } else {
+        db.get('SELECT rowid AS id, username, name FROM users WHERE rowid = ?', [ row.user_id ], function(err, row) {
+          if (err) { return next(err); }
+    
+          // TODO: Handle undefined row.
+          var user = {
+            id: row.id.toString(),
+            username: row.username,
+            displayName: row.name
+          };
+          req.login(user, function(err) {
+            if (err) { return next(err); }
+            res.redirect('/');
+          });
+        });
+      }
+    });
+    
   });
 
 router.get('/logout', function(req, res, next) {
