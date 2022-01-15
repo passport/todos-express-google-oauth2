f9b0667df462fdebe07e9baa7657ad1ee9d8c7a5
Jared Hanson <jaredhanson@gmail.com>
2022-01-15 06:44:36 -0800

    Update app with latest dependencies.

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 require('dotenv').config();
 
+var createError = require('http-errors');
 var express = require('express');
-var passport = require('passport');
-var csrf = require('csurf');
 var path = require('path');
 var cookieParser = require('cookie-parser');
+var session = require('express-session');
+var csrf = require('csurf');
+var passport = require('passport');
 var logger = require('morgan');
+
+// pass the session to the connect sqlite3 module
+// allowing it to inherit from session.Store
+var SQLiteStore = require('connect-sqlite3')(session);
 
 var indexRouter = require('./routes/index');
 var authRouter = require('./routes/auth');
@@ -23,17 +29,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-// session setup
-//
-// This sequence of middleware is necessary for login sessions.  The first
-// middleware loads session data and makes it available at `req.session`.  The
-// next lines initialize Passport and authenticate the request based on session
-// data.  If session data contains a logged in user, the user is set at
-// `req.user`.
-app.use(require('express-session')({ secret: 'keyboard cat', resave: false, saveUninitialized: false }));
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false, // don't save session if unmodified
+  saveUninitialized: false, // don't create session until something stored
+  store: new SQLiteStore({ db: 'sessions.db', dir: 'var/db' })
+}));
 app.use(csrf());
-app.use(passport.initialize());
-app.use(passport.session());
+app.use(passport.authenticate('session'));
 app.use(function(req, res, next) {
   var msgs = req.session.messages || [];
   res.locals.messages = msgs;
@@ -48,5 +51,21 @@ app.use(function(req, res, next) {
 
 app.use('/', indexRouter);
 app.use('/', authRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
 
 module.exports = app;
